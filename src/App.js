ac5df83835ca20745cec808ab0ec1a4d49bf62c2
ujin <rezashahtrader@gmail.com>
2023-05-30 06:57:36 +0330

    .

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+          <span style={{color: 'white'}}></span>
     </div>
   );
-  
+
 }
 
 export default App;

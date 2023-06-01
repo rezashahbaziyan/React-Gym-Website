cd027e0d09af243a5d2e451c42ec2c88d8c8ec29
ujin <rezashahtrader@gmail.com>
2023-06-01 22:04:32 +0330

    .

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <Header/>
             <div className="the-best-ad">
               <div></div>
-              <span>the best fitness club</span>
+              <span>the best fitness club in the town</span>
             </div>
         </div>
         <div className="right-h">Right Side</div>

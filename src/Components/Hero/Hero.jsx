f8f6bd8e82b7bdce62d80d6f1305d601b006742b
ujin <rezashahtrader@gmail.com>
2023-06-02 21:08:02 +0330

    .

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,9 +7,20 @@ const Hero = () => {
         <div className="left-h">
             
             <Header/>
+            {/* the best ad */}
             <div className="the-best-ad">
               <div></div>
               <span>the best fitness club in the town</span>
+            </div>
+            {/* Hero Heading */}
+            <div className="hero-text">
+              <div>
+                <span>Shape</span>
+                <span>Your</span>
+              </div>
+              <div>
+                <span>Ideal body</span>
+              </div>
             </div>
         </div>
         <div className="right-h">Right Side</div>

51441515f2207f1977c0bd886ec43b51c9b708e6
ujin <rezashahtrader@gmail.com>
2023-06-06 13:21:02 +0330

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Hero from './Components/Hero/Hero';
+import Programs from './Components/progeams/Programs';
 function App() {
   return (
     <div className="App">
       {/* <span style={{color: 'white'}}></span> */}
       <Hero />
+      <Programs />
     </div>
   );
 }

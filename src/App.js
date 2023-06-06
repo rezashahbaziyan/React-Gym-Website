1b2b7541e0455197be5dc4b5151a4294103f24d0
ujin <rezashahtrader@gmail.com>
2023-06-06 13:47:46 +0330

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Hero from './Components/Hero/Hero';
-import Programs from './Components/progeams/Programs';
+import Programs from './Components/programs/Programs';
 function App() {
   return (
     <div className="App">

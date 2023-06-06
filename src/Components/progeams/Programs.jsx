19b2e1c0c95697ec72b3ddabb18ba9cc532fdd40
ujin <rezashahtrader@gmail.com>
2023-06-06 13:38:07 +0330

    .

--- src/Components/progeams/Programs.jsx
+++ src/Components/progeams/Programs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Programs.css";
 import {programsData} from '../../data/programsData';
+import RightArrow from '../../assets/rightArrow.png';
 
 const Programs = () => {
   return (
@@ -18,6 +19,10 @@ const Programs = () => {
                     {program.image}
                     <span>{program.heading}</span>
                     <span>{program.details}</span>
+                    <div className="join-now">
+                        <span>Join Now</span>
+                        <img src={RightArrow} alt="" />
+                    </div>
                 </div>
             ))}
         </div>

97965a94610010fbae9630657f0ed2d3e371e082
ujin <rezashahtrader@gmail.com>
2023-06-07 15:39:06 +0330

    .

--- src/Components/Plans/Plans.jsx
+++ src/Components/Plans/Plans.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Plans.css";
 import {plansData} from '../../data/plansData';
+import whiteTick from '../../assets/whiteTick.png';
 
 const Plans = () => {
   return (
@@ -10,15 +11,30 @@ const Plans = () => {
             <span>YOUR JOURNEY</span>
             <span className='stroke-text'>NOW WITHUS</span>
         </div>
+
         {/* plans card */}
         <div className="plans">
-            {plansData.map((plan,i)=>{
+            {plansData.map((plan,i)=>(
                 <div className="plan" key={i}>
                     {plan.icon}
                     <span>{plan.name}</span>
                     <span>$ {plan.price}</span>
+                    
+                    <div className="features">
+                        {plan.features.map((feature,i)=>(
+                            <div className="feature">
+                                <img src={whiteTick} alt="" />
+                                <span key={i}>{feature}</span>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div>
+                        <span>See more benefits -></span>
+                    </div>
+                    <button className='btn'>Join Now</button>
                 </div>
-            })}
+            ))}
         </div>
     </div>
   )

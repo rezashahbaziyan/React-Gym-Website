47371e898ed6e06efd393bd9b4149d4d1a12e276
ujin <rezashahtrader@gmail.com>
2023-06-08 22:41:07 +0330

    .

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,12 +10,14 @@ const Footer = () => {
     <div className="footer-container">
         <hr />
         <div className="footer">
-            <img src={Github} alt="" />
-            <img src={Instagram} alt="" />
-            <img src={LinkedIn} alt="" />
-        </div>
-        <div className="logo-f">
-            <img src={Logo} alt="" />
+            <div className="social-links">
+                <img src={Github} alt="" />
+                <img src={Instagram} alt="" />
+                <img src={LinkedIn} alt="" />
+            </div>
+            <div className="logo-f">
+                <img src={Logo} alt="" />
+            </div>
         </div>
     </div>
   )

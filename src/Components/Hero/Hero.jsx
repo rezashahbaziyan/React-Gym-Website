03fdb3a9b8d9c1dff170236adccd8dda196a54a4
ujin <rezashahtrader@gmail.com>
2023-06-10 18:03:39 +0330

    .

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
-import CountUp from "react-countup"
+import CountUp from "react-countup"; 
 
 import {motion} from 'framer-motion';
 const Hero = () => {

3f3005b55c87d24b8435aa286d30084ec1822961
Reza shahbaziyan <92777471+rezashahbaziyan@users.noreply.github.com>
2023-06-22 21:57:33 +0330

    .

--- src/App.js
+++ src/App.js
@@ -11,12 +11,8 @@ import './App.css';
 import Footer from './Components/Footer/Footer';
 import Hero from './Components/Hero/Hero';
 import Join from './Components/Join/Join';
-import Plans from './Components/Plans/Plans';
-import Reasons from './Components/Reasons/Reasons';
-import Testimonials from './Components/Testimonials/Testimonials';
-import Programs from './Components/programs/Programs';
-
-function App() {
+ 
+function App() {ion App() {
   return (
     <div className="App">
       {/* <span style={{color: 'white'}}></span> */}

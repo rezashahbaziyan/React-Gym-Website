0b6db258405909ade2e6b59b20272420bc5ea195
Reza shahbaziyan <92777471+rezashahbaziyan@users.noreply.github.com>
2023-06-22 21:57:02 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import Plans from './Components/Plans/Plans';
 import Reasons from './Components/Reasons/Reasons';
 import Testimonials from './Components/Testimonials/Testimonials';
 import Programs from './Components/programs/Programs';
+
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import Hero from './Components/Hero/Hero';
+import Join from './Components/Join/Join';
+import Plans from './Components/Plans/Plans';
+import Reasons from './Components/Reasons/Reasons';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Programs from './Components/programs/Programs';
+
 function App() {
   return (
     <div className="App">
